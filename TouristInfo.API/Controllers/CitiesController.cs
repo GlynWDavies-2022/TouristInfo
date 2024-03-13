bfd3e7224237e1f00369ce8d39c488d9be3df884
Glyn Davies <glynwdavies@gmail.com>
2024-03-13 13:12:09 +0000

    Created new API project.

---
+++ TouristInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TouristInfo.API.Controllers;
+
+[ApiController]
+[Route("api/cities")]
+public class CitiesController : ControllerBase
+{
+    [HttpGet]
+    public JsonResult GetCities()
+    {
+        return new JsonResult(
+            new List<object>
+            {
+                new { CityName = "New York" },
+                new { CityName = "Paris" },
+                new { CityName = "London" }
+            }
+        );
+    }
+}
