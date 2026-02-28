e42e72eb1b6bf6a2ebb9588fb6b473453d436cd6
Glyn Davies <glynwdavies@gmail.com>
2026-02-28 18:39:33 +0000

    Added documentation comments.

--- TouristInfo.API/Program.cs
+++ TouristInfo.API/Program.cs
@@ -1,14 +1,23 @@
+// ------------------------------------------------------------------------------------------------
+// Application Starting Point
+// ------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------
+// Service Container
+// ------------------------------------------------------------------------------------------------
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// ------------------------------------------------------------------------------------------------
+// Middleware Pipeline
+// ------------------------------------------------------------------------------------------------
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -21,3 +30,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+// ------------------------------------------------------------------------------------------------
