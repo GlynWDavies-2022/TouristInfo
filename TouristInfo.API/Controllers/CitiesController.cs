5a762f5b6d3da1cb18c544723139afc43234aecf
Glyn Davies <glynwdavies@gmail.com>
2026-02-28 19:35:23 +0000

    Created cities controller.

---
+++ TouristInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TouristInfo.API.Controllers;
+
+[ApiController]
+[Route("api/cities")]
+public class CitiesController : ControllerBase
+{
+    [HttpGet]
+    public JsonResult GetCities()
+    {
+        return new JsonResult(
+            new[]
+            {
+                new { Id = 1, Name = "New York" },
+                new { Id = 2, Name = "Antwerp" }
+            });
+    }
+}
