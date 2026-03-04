8fad85c362942e05598ff1f18db86244f2776864
Glyn Davies <glynwdavies@gmail.com>
2026-03-04 08:26:21 +0000

    Created new POST endpoing

--- TouristInfo.API/Controllers/PointsOfInterestController.cs
+++ TouristInfo.API/Controllers/PointsOfInterestController.cs
@@ -8,6 +8,36 @@ namespace TouristInfo.API.Controllers;
 [Route("api/cities/{cityId}/pointsofinterest")]
 public class PointsOfInterestController : ControllerBase
 {
+    // [FromBody] isn't mandatory for complex types, but it's a good practice to be explicit about where the data is coming from.
+
+    [HttpPost]
+    public ActionResult<PointOfInterestDTO> CreatePointOfInterest(int cityId, [FromBody] PointOfInterestForCreationDTO pointOfInterest)
+    {
+        var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+        if (city == null)
+        {
+            return NotFound();
+        }
+
+        var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+
+        var finalPointOfInterest = new PointOfInterestDTO
+        {
+            Id = ++maxPointOfInterestId,
+            Name = pointOfInterest.Name,
+            Description = pointOfInterest.Description
+        };
+
+        city.PointsOfInterest.Add(finalPointOfInterest);
+
+        return CreatedAtRoute(
+            "GetPointOfInterest", 
+            new { cityId, pointOfInterestId = finalPointOfInterest.Id } // cityId = cityId simplified  
+            , finalPointOfInterest
+        );
+    }
+
     [HttpGet]
     public ActionResult<IEnumerable<PointOfInterestDTO>> GetPointsOfInterest(int cityId)
     {
@@ -22,7 +52,7 @@ public class PointsOfInterestController : ControllerBase
     }
 
     [HttpGet]
-    [Route("{pointOfInterestId}")]
+    [Route("{pointOfInterestId}", Name = "GetPointOfInterest")]
     public ActionResult<PointOfInterestDTO> GetPointOfInterest(int cityId, int pointOfInterestId)
     {
         var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
